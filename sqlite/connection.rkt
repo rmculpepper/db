aa07388a3121fd3cad9ed9c8fada2eadf5a08f11
Ryan Culpepper <ryanc@racket-lang.org>
2011-03-21 20:46:44 -0600

    improved error messages

--- sqlite/connection.rkt
+++ sqlite/connection.rkt
@@ -1,4 +1,4 @@
-;; Copyright 2010 Ryan Culpepper
+;; Copyright 2011 Ryan Culpepper
 ;; Released under the terms of the modified BSD license (see the file
 ;; COPYRIGHT for terms).
 
@@ -124,18 +124,18 @@
         (with-lock
          (let ([db (get-db fsym)]
                [stmt (send pst get-stmt)])
-           (handle-status fsym (sqlite3_reset stmt))
-           (handle-status fsym (sqlite3_clear_bindings stmt))
+           (handle-status fsym (sqlite3_reset stmt) db)
+           (handle-status fsym (sqlite3_clear_bindings stmt) db)
            (for ([i (in-naturals 1)]
                  [param (in-list params)])
-             (load-param fsym stmt i param))
+             (load-param fsym db stmt i param))
            (let* ([info
                    (for/list ([i (in-range (sqlite3_column_count stmt))])
                      `((name ,(sqlite3_column_name stmt i))
                        (decltype ,(sqlite3_column_decltype stmt i))))]
-                  [rows (step* fsym stmt)])
-             (handle-status fsym (sqlite3_reset stmt))
-             (handle-status fsym (sqlite3_clear_bindings stmt))
+                  [rows (step* fsym db stmt)])
+             (handle-status fsym (sqlite3_reset stmt) db)
+             (handle-status fsym (sqlite3_clear_bindings stmt) db)
              (values info rows)))))
       (let-values ([(init combine finalize info)
                     (collector info #f)])
@@ -148,7 +148,7 @@
               [else
                (make-SimpleResult "")])))
 
-    (define/private (load-param fsym stmt i param)
+    (define/private (load-param fsym db stmt i param)
       (handle-status
        fsym
        (cond [(integer? param)
@@ -162,13 +162,14 @@
              [(sql-null? param)
               (sqlite3_bind_null stmt i)]
              [else
-              (error fsym "bad parameter: ~e" param)])))
+              (error fsym "bad parameter: ~e" param)])
+       db))
 
-    (define/private (step* fsym stmt)
-      (let ([c (step fsym stmt)])
-        (if c (cons c (step* fsym stmt)) null)))
+    (define/private (step* fsym db stmt)
+      (let ([c (step fsym db stmt)])
+        (if c (cons c (step* fsym db stmt)) null)))
 
-    (define/private (step fsym stmt)
+    (define/private (step fsym db stmt)
       (let ([s (sqlite3_step stmt)])
         (cond [(= s SQLITE_DONE) #f]
               [(= s SQLITE_ROW)
@@ -190,7 +191,7 @@
                                         [else
                                          (error 'query* "unknown column type: ~e" type)]))))
                  vec)]
-              [else (handle-status fsym s)])))
+              [else (handle-status fsym s db)])))
 
     (define/public (prepare-multiple stmts)
       (for/list ([stmt stmts])
@@ -199,13 +200,14 @@
     (define/private (prepare1 fsym sql)
       (with-lock
        ;; no time between sqlite3_prepare and table entry
-       (let-values ([(stmt prep-status tail)
+       (let-values ([(db) (get-db fsym)]
+                    [(stmt prep-status tail)
                      (sqlite3_prepare_v2 (get-db fsym) sql)])
          (when (string=? sql tail)
            (error fsym "SQL syntax error in ~e" tail))
          (when (not (zero? (string-length tail)))
            (error fsym "multiple SQL statements given: ~e" tail))
-         (when (handle-status fsym prep-status)
+         (when (handle-status fsym prep-status db)
            (or stmt
                (error fsym "internal error in prepare")))
          (let ([pst (new prepared-statement%
@@ -245,39 +247,41 @@
 ;; handle-status : symbol integer -> integer
 ;; Returns the status code if no error occurred, otherwise
 ;; raises an exception with an appropriate message.
-(define (handle-status who s)
+(define (handle-status who s [db #f])
   (if (or (= s SQLITE_OK)
           (= s SQLITE_ROW)
           (= s SQLITE_DONE))
       s
-      (error who "~a" (lookup-status-message s))))
+      (error who "~a" (lookup-status-message s db))))
 
-;; lookup-status-message : (U db #f) integer -> string
-(define (lookup-status-message s)
-  (cdr (assoc s
-              `([,SQLITE_ERROR . "Generic error"]
-                [,SQLITE_INTERNAL . "An internal logic error in SQLite"]
-                [,SQLITE_PERM . "Access permission denied"]
-                [,SQLITE_ABORT . "Callback routine requested an abort"]
-                [,SQLITE_BUSY . "The database file is locked"]
-                [,SQLITE_LOCKED . "table in the database is locked"]
-                [,SQLITE_NOMEM . "A malloc() failed"]
-                [,SQLITE_READONLY . "Attempt to write a readonly database"]
-                [,SQLITE_INTERRUPT . "Operation terminated by sqlite3_interrupt()"]
-                [,SQLITE_IOERR . "Some kind of disk I/O error occurred"]
-                [,SQLITE_CORRUPT . "The database disk image is malformed"]
-                [,SQLITE_NOTFOUND . "(Internal Only) Table or record not found"]
-                [,SQLITE_FULL . "Insertion failed because database is full"]
-                [,SQLITE_CANTOPEN . "Unable to open the database file"]
-                [,SQLITE_PROTOCOL . "Database lock protocol error"]
-                [,SQLITE_EMPTY . "Database is empty"]
-                [,SQLITE_SCHEMA . "The database schema changed"]
-                [,SQLITE_TOOBIG . "Too much data for one row of a table"]
-                [,SQLITE_CONSTRAINT . "Abort due to contraint violation"]
-                [,SQLITE_MISMATCH . "Data type mismatch"]
-                [,SQLITE_MISUSE . "Library used incorrectly"]
-                [,SQLITE_NOLFS . "Uses OS features not supported on host"]
-                [,SQLITE_AUTH . "Authorization denied"]
-                [,SQLITE_FORMAT . "Auxiliary database format error"]
-                [,SQLITE_RANGE . "2nd parameter to sqlite3_bind out of range"]
-                [,SQLITE_NOTADB . "File opened that is not a database file"]))))
+;; lookup-status-message : integer db/#f -> string
+(define (lookup-status-message s db)
+  (if (and (eq? s SQLITE_ERROR) db)
+      (sqlite3_errmsg db)
+      (cdr (assoc s
+                  `([,SQLITE_ERROR . "Generic error, perhaps call errmsg?"]
+                    [,SQLITE_INTERNAL . "An internal logic error in SQLite"]
+                    [,SQLITE_PERM . "Access permission denied"]
+                    [,SQLITE_ABORT . "Callback routine requested an abort"]
+                    [,SQLITE_BUSY . "The database file is locked"]
+                    [,SQLITE_LOCKED . "table in the database is locked"]
+                    [,SQLITE_NOMEM . "A malloc() failed"]
+                    [,SQLITE_READONLY . "Attempt to write a readonly database"]
+                    [,SQLITE_INTERRUPT . "Operation terminated by sqlite3_interrupt()"]
+                    [,SQLITE_IOERR . "Some kind of disk I/O error occurred"]
+                    [,SQLITE_CORRUPT . "The database disk image is malformed"]
+                    [,SQLITE_NOTFOUND . "(Internal Only) Table or record not found"]
+                    [,SQLITE_FULL . "Insertion failed because database is full"]
+                    [,SQLITE_CANTOPEN . "Unable to open the database file"]
+                    [,SQLITE_PROTOCOL . "Database lock protocol error"]
+                    [,SQLITE_EMPTY . "Database is empty"]
+                    [,SQLITE_SCHEMA . "The database schema changed"]
+                    [,SQLITE_TOOBIG . "Too much data for one row of a table"]
+                    [,SQLITE_CONSTRAINT . "Abort due to contraint violation"]
+                    [,SQLITE_MISMATCH . "Data type mismatch"]
+                    [,SQLITE_MISUSE . "Library used incorrectly"]
+                    [,SQLITE_NOLFS . "Uses OS features not supported on host"]
+                    [,SQLITE_AUTH . "Authorization denied"]
+                    [,SQLITE_FORMAT . "Auxiliary database format error"]
+                    [,SQLITE_RANGE . "2nd parameter to sqlite3_bind out of range"]
+                    [,SQLITE_NOTADB . "File opened that is not a database file"])))))

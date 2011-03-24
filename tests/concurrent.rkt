d81691b348e39ec822b977b542265336f4dae8a0
Ryan Culpepper <ryanc@racket-lang.org>
2011-03-24 00:53:29 -0600

    deleted useless test

--- tests/concurrent.rkt
+++ tests/concurrent.rkt
@@ -1,4 +1,4 @@
-;; Copyright 2000-2010 Ryan Culpepper
+;; Copyright 2000-2011 Ryan Culpepper
 ;; Released under the terms of the modified BSD license (see the file
 ;; COPYRIGHT for terms).
 
@@ -20,15 +20,6 @@
       ((postgresql) pg)
       ((mysql sqlite3) my)
       (else (error 'sql "unknown database system: ~e" dbsystem))))
-
-  (define (make-slow-output-port out pause? limit)
-    (make-output-port 'slow-port
-                      out
-                      (lambda (buf start end ok-buffer? ok-break?)
-                        (when pause? (sleep 0.01))
-                        (let ([end (min end (+ start limit))])
-                          (write-bytes-avail buf out start end)))
-                      (lambda () (close-output-port out))))
 
   (define (test-concurrency workers)
     (test-case (format "lots of threads (~s)" workers)
@@ -57,23 +48,4 @@
       ;; Tests whether connections are properly locked.
       (test-concurrency 1)
       (test-concurrency 2)
-      (test-concurrency 20)
-
-      (test-case "threads with small-chunk ports"
-        ;; Tests whether connections work just as well if the connections
-        ;; dribble bytes slowly.
-        ;; Not sure this test is useful.
-        (parameterize ((testing-connection-mixin
-                        (lambda (%)
-                          (class %
-                            (define/override (attach-to-ports in out)
-                              (super attach-to-ports
-                                     in 
-                                     (make-slow-output-port out #f 1)))
-                            (super-new)))))
-          (call-with-connection
-           (lambda (c)
-             (query-exec c "create temporary table play_numbers (n integer)")
-             (for-each thread-wait
-                       (map thread
-                            (map (mk-worker c 5) (build-list 4 add1)))))))))))
+      (test-concurrency 20))))

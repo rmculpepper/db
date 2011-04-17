1cda0333c5ec385ecd4def5805733b5bac93d55c
Ryan Culpepper <ryanc@racket-lang.org>
2011-04-16 19:21:25 -0600

    updated info

--- info.rkt
+++ info.rkt
@@ -3,9 +3,12 @@
 ;; See the file COPYRIGHT for details.
 
 #lang setup/infotab
+
 (define name "db")
 (define compile-omit-paths
   '("tests"))
+(define scribblings '(("scribblings/db.scrbl" (multi-page))))
+
 (define blurb
   '("A database interface for functional programmers."
     "This library provides a high-level, functional interface to PostgreSQL, "
@@ -13,7 +16,10 @@
     "PostgreSQL and MySQL support is implemented in pure Racket. "
     "The optional SQLite and ODBC support requires the appropriate "
     "native client libraries."))
-(define scribblings '(("scribblings/db.scrbl" (multi-page))))
+(define release-notes
+  '("Initial release."))
 (define categories '(io net))
 (define can-be-loaded-with 'all)
+(define primary-file "main.rkt")
 (define required-core-version "5.1")
+(define repositories '("4.x"))
